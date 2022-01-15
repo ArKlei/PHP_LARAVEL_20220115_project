801a69ceaaf92ac748670128783822937882ebee
ArKlei <arunas.kleinas@gmail.com>
2022-01-15 11:01:34 +0200

    Created groups of routes for Models/Object classes
    19. Routes> web.php sukuriame route grupę objektų klasėms Clients ir Companies, kiekvienoje nurodant route'us visiems metodams: create, store, edit, update, show, destroy.
    20. Svarbu nurodyti vardus kaip "->name('modelioPavadinimasVienaskaita.metodas') - pvz. ->name('client.create'), kad nereiktų nurodyti ilgo kontrolerio kelio, pvz. 'App\Http\Controllers\ClientController@create'.
    21. Metodams kai duomenys siunčiami į duomenų bazę ir yra saugiau, naudojamas metodas GET (Laravel standartas), kai gaunami iš jos - POST.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Pavienis kelias
 Route::get('/', function () {
     return view('welcome');
-});
+  });
+  
+  //Objektų klasė Clients
+  Route::prefix('clients')->group(function() {
+  
+    //Index. /clients
+    Route::get('', 'App\Http\Controllers\ClientController@index')->name('client.index');
+    
+    //Create
+    Route::get('create', 'App\Http\Controllers\ClientController@create')->name('client.create');
+    Route::post('store', 'App\Http\Controllers\ClientController@store' )->name('client.store');
+    
+    // Edit form, id
+    Route::get('edit/{client}', 'App\Http\Controllers\ClientController@edit')->name('client.edit');
+    Route::post('update/{client}', 'App\Http\Controllers\ClientController@update')->name('client.update');
+    
+    //Delete
+    Route::post('destroy/{client}', 'App\Http\Controllers\ClientController@destroy' )->name('client.destroy');
+    
+    //Show
+    Route::get('show/{client}', 'App\Http\Controllers\ClientController@show')->name('client.show');
+
+
+    //Objektų klasė Companies
+    Route::prefix('companies')->group(function() {
+  
+        //Index. /companies
+        Route::get('', 'App\Http\Controllers\CompanyController@index')->name('company.index');
+        
+        //Create
+        Route::get('create', 'App\Http\Controllers\CompanyController@create')->name('company.create');
+        Route::post('store', 'App\Http\Controllers\CompanyController@store' )->name('company.store');
+        
+        // Edit form, id
+        Route::get('edit/{company}', 'App\Http\Controllers\CompanyController@edit')->name('company.edit');
+        Route::post('update/{company}', 'App\Http\Controllers\CompanyController@update')->name('company.update');
+        
+        //Delete
+        Route::post('destroy/{company}', 'App\Http\Controllers\CompanyController@destroy' )->name('company.destroy');
+        
+        //Show
+        Route::get('show/{company}', 'App\Http\Controllers\CompanyController@show')->name('company.show');
