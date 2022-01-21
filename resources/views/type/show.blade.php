993c5e57236a089bf599e50e6ab34472d3fa58b2
ArKlei <arunas.kleinas@gmail.com>
2022-01-21 22:51:03 +0200

    Update show.blade.php

--- resources/views/type/show.blade.php
+++ resources/views/type/show.blade.php
@@ -4,93 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Show clients's company by ID</title>
+    <title>Show Type</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-<style>
-#mySidenav a {
-  position: absolute;
-  left: -140px;
-  transition: 0.3s;
-  padding: 15px;
-  width: 200px;
-  text-decoration: none;
-  font-size: 20px;
-  color: white;
-  border-radius: 0 5px 5px 0;
-}
-
-#mySidenav a:hover {
-  left: 0;
-}
-
-#main {
-  top: 20px;
-  background-color: #04AA6D;
-}
-
-#create {
-  top: 80px;
-  background-color: #2196F3;
-}
-
-#company {
-  top: 140px;
-  background-color: #f44336;
-}
-
-#create_company {
-  top: 200px;
-  background-color: #555
-}
-</style>
 </head>
 <body>
-
-<div id="mySidenav" class="sidenav">
-  <a href="{{route('main')}}" id="main">Main</a>
-  <a href="{{route('client.create')}}" id="create">Add client</a>
-  <a href="{{route('company.index')}}" id="company">Companies</a>
-  <a href="{{route('company.create')}}" id="create_company">Add company</a>
-</div>
     <div class="container">
-        <p><h1 style="text-align:center; font-size:50px; color:gold"> {{$company->type}} {{$company->name}}  </h1><p>
-        <p>Id : {{$company->id}}</p>
-        <p>Name : {{$company->name}}</p>
-        <p>Description : {{$company->description}}</p>
-        <p>
+        <h2> {{$type->name}}  </h2>
+            <p>Id : {{$type->id}}</p>
+        <p>Name : {{$type->name}}</p>
+        <p>Short Name : {{$type->short_name}}</p>
+        <p>Description : {{$type->description}}</p>
 
-        @if(count($company->companyClients) == 0)
-          <p>No Clients in this Company</p>
-        @else
-        <table class="table table-stripped">
-          <tr>
-            <td>ID</td>
-            <td>Name</td>
-            <td>surname</td>
-            <td>Image</td>
-            <td>Action</td>
-          </tr>
-          @foreach ($company->companyClients as $client)
-          <tr>
-            <td>{{$client->id}}</td>
-            <td>{{$client->name}}</td>
-            <td>{{$client->surname}}</td>
-            <td><img src='{{$client->image_url}}' alt='{{$client->name}}' width="150" height="auto"/></td>
-            <td>
-                <form method="post" action='{{route('client.destroy',[$client])}}''>
-                @csrf
-                <button class="btn btn-danger" type="submit">Delete</button>
-            </form></td>
-          </tr>
-          @endforeach
-        </table>
-        @endif
-        <form method="post" action='{{route('company.destroy', [$company])}}'>
+        {{-- $company->companyClients->name xxxx ???? --}}
+        @if(count($type->typeCompanies) == 0) 
+            <p>There is no companies </p>
+        @else 
+            <table class="table table-striped">
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Description</th>
+                </tr>
+            @foreach ($type->typeCompanies as $company)
+                <tr>
+                    <td>{{$company->id}}</td>
+                    <td>{{$company->name}}</td>
+                    <td>{{$company->description}}</td>
+                </tr>
+            @endforeach
+            </table>
+        @endif    
+
+        <form method="post" action='{{route('type.destroy', [$type])}}''>
             @csrf
-            <button class="btn btn-danger" type="submit">Delete company from database</button>
-        </form><p>
-        <p><a class="btn btn-secondary" style="width:100px" href="{{route('company.index')}}">Back</a>
+            <button class="btn btn-danger" type="submit">Delete</button>
+        </form>
+        <a class="btn btn-secondary" href="{{route('type.index')}}">Back</a>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
