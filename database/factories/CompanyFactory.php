be28a1b81c44ac8798e1bb8b7e1aa3c1e51e7949
ArKlei <arunas.kleinas@gmail.com>
2022-01-21 21:57:15 +0200

    Updated Company Factory file
    return [
                // // ID
                // name(string)
                // type(string)
                // description(string)
    
                'name' => $this->faker->company(),
                'type_id' => $this->faker->numberBetween(1,4)
                'description' => $this->faker->paragraph(15)
            ];

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -13,10 +13,22 @@ class CompanyFactory extends Factory
      */
     public function definition()
     {
+        
         return [
+            // // ID
+            // name(string)
+            // type(string)
+            // description(string)
+
             'name' => $this->faker->company(),
-            'type' =>'Juridinis asmuo',
-            'description' => $this->faker->paragraph(15),
+            'type_id' => $this->faker->numberBetween(1,4)
+            'description' => $this->faker->paragraph(15)
         ];
+        
+        //return [
+        //    'name' => $this->faker->company(),
+        //    'type' =>'Juridinis asmuo',
+        //    'description' => $this->faker->paragraph(15),
+        //];
     }
 }
