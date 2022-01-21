c38281b09bd02fb5f0cfa401393913876e25e7a0
ArKlei <arunas.kleinas@gmail.com>
2022-01-21 21:53:52 +0200

    Updated Database seeder file
    Type seeder įkeltas pirmas
     $this->call([
                TypeSeeder::class,
                CompanySeeder::class,
                ClientSeeder::class
            ]);

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         //Client::factory()->count(30)->create();
         //Company::factory()->count(10)->create();
         $this->call([
-            
+            TypeSeeder::class,
             CompanySeeder::class,
             ClientSeeder::class
         ]);
