30d65d1e388c4d5e5239da24cb2289bf5a9b9a22
ArKlei <arunas.kleinas@gmail.com>
2022-01-21 22:24:04 +0200

    Update TypeController.php

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -6,6 +6,9 @@ use App\Models\Type;
 use App\Http\Requests\StoreTypeRequest;
 use App\Http\Requests\UpdateTypeRequest;
 
+use Illuminate\Http\Request;
+
+
 class TypeController extends Controller
 {
     /**
@@ -18,7 +21,6 @@ class TypeController extends Controller
         $types = Type::all();
         return view('type.index',['types'=>$types]);
     }
-    
 
     /**
      * Show the form for creating a new resource.
@@ -27,7 +29,7 @@ class TypeController extends Controller
      */
     public function create()
     {
-        //
+        return view('type.create');
     }
 
     /**
@@ -36,9 +38,18 @@ class TypeController extends Controller
      * @param  \App\Http\Requests\StoreTypeRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreTypeRequest $request)
+    public function store(Request $request)
     {
-        //
+        $type = new Type;
+
+        $type->name = $request->type_name;
+        $type->short_name = $request->type_shortname;
+        $type->description = $request->type_description;
+
+        $type->save();
+
+        return redirect()->route('type.index');
+
     }
 
     /**
@@ -49,7 +60,7 @@ class TypeController extends Controller
      */
     public function show(Type $type)
     {
-        //
+        return view('type.show', ['type' => $type]);
     }
 
     /**
@@ -60,7 +71,7 @@ class TypeController extends Controller
      */
     public function edit(Type $type)
     {
-        //
+        return view('type.edit', ['type' => $type]);
     }
 
     /**
@@ -70,9 +81,15 @@ class TypeController extends Controller
      * @param  \App\Models\Type  $type
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateTypeRequest $request, Type $type)
+    public function update(Request $request, Type $type)
     {
-        //
+        $type->name = $request->type_name;
+        $type->short_name = $request->type_shortname;
+        $type->description = $request->type_description;
+
+        $type->save();
+
+        return redirect()->route('type.index');
     }
 
     /**
@@ -83,6 +100,13 @@ class TypeController extends Controller
      */
     public function destroy(Type $type)
     {
-        //
+        $companies = $type->typeCompanies; //visas kompanijas kurios priklaso tipui
+
+        if(count($companies) != 0) {
+            return redirect()->route('type.index')->with('error_message','Delete is not possible because type has companies');
+        }
+
+        $type->delete();
+         return redirect()->route('type.index')->with('success_message', 'Everything is fine');
     }
 }
