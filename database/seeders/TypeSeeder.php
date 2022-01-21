59d1cdf4b2fd444a94029f8e378c68ea952cca57
ArKlei <arunas.kleinas@gmail.com>
2022-01-21 21:50:58 +0200

    Type seeder run function filled
    Preaped for types companies information to include into database

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -13,6 +13,30 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //name, short_name, description
+        //Mažoji bendrija, MB, ribota atsakomybė
+        //Uždaroji akcinė bendrovė, UAB, ribota atsakomybė
+        //Individuali įmonė, IĮ, neribota atsakomybė
+        //Akcinė bendrovė, AB, ribota atsakomybė
+        DB::table('types')->insert([
+            'name' => 'Mažoji bendrija',
+            'short_name' => 'MB',
+            'description' => 'ribota atsakomybė',
+        ]);
+        DB::table('types')->insert([
+            'name' => 'Uždaroji akcinė bendrovė',
+            'short_name' => 'UAB',
+            'description' => 'ribota atsakomybė',
+        ]);
+        DB::table('types')->insert([
+            'name' => 'Individuali įmonė',
+            'short_name' => 'IĮ',
+            'description' => 'neribota atsakomybė',
+        ]);
+        DB::table('types')->insert([
+            'name' => 'Akcinė bendrovė',
+            'short_name' => 'AB',
+            'description' => 'ribota atsakomybė',
+        ]);
     }
 }
